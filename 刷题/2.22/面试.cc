29127bea21d0f30fbb8e6cea331225dea575b078
yangnewhub <yangking6666@protonmail.com>
2025-02-24 12:22:05 +0800

    前序非递归

--- 刷题/2.22/面试.cc
+++ 刷题/2.22/面试.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 class ListNode
@@ -22,6 +23,20 @@ void dfs(ListNode* root)
 	dfs(root->_left);
 	dfs(root->_right);
 }
+
+void mydfs(ListNode* root)
+{
+	stack<ListNode*> s;
+	s.push(root);
+	while(!s.empty())
+	{
+		ListNode* p=s.top();
+		s.pop();
+		std::cout<<p->_val<<std::endl;
+		if(p->_right) s.push(p->_right);
+		if(p->_left) s.push(p->_left);
+	}
+}
 int main()
 {
 	ListNode* head = new ListNode(0);
@@ -34,4 +49,8 @@ int main()
 	cur->_left = new ListNode(3);
 	cur->_right = new ListNode(2);
 	dfs(head);
+
+
+	std::cout<<"-------------------------------"<<std::endl;
+	mydfs(head);
 }
